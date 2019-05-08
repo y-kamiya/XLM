aedabdffc65f57b202bd8f82158c86dd1bbc05c2
Yuji Kamiya <y.kamiya0@gmail.com>
2019-05-08 09:36:57 +0900

    script for setup env

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget https://dl.fbaipublicfiles.com/XLM/vocab_enfr
+wget https://dl.fbaipublicfiles.com/XLM/codes_enfr
+wget https://dl.fbaipublicfiles.com/XLM/mlm_enfr_1024.pth
+
+git clone https://github.com/NVIDIA/apex
+pushd apex
+sudo chmod 777 /opt/anaconda3/lib/python3.7/site-packages
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
+popd
+
+./get-data-nmt.sh --src en --tgt fr --reload_codes codes_enfr --reload_vocab vocab_enfr
